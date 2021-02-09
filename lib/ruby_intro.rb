650c937c0883f20ef9bde57ef38ed8016af59554
EC2 Default User <ec2-user@ip-172-31-15-191.us-east-2.compute.internal>
2021-02-09 23:23:09 +0000

    First Commit

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,32 +4,88 @@
 
 def sum arr
   # YOUR CODE HERE
+  array_length = arr.length
+  return 0 if array_length == 0
+  arr.inject(0, :+)
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
+  if arr.length== 0
+   return 0
+  end
+  if arr.length== 1
+   return arr[-1]
+  end
+  temp = arr.sort
+  return (temp[-1] + temp[-2])
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  if arr.length== 1
+   return FALSE
+  end
+  arr.each do |i|
+    arr.each do |y|
+      if y == i
+        next
+      end
+      if (i + y) == n
+        return TRUE
+      end
+    end
+  end
+  return FALSE
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  # YOUR CODE 
+  "Hello, " + name
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  temp = s.downcase
+  if (temp[0]=~/[bcdfghjklmnpqrstvwxyz]/)
+    return TRUE
+  end
+  return FALSE
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  if s =~ /^[0-1]/
+    return s.to_i(2) % 4 == 0 
+  end
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+  def initialize(isbn,price)
+    raise ArgumentError if isbn == nil || isbn.length == 0
+    raise ArgumentError if price <= 0
+    @isbn = isbn
+    @price = price
+  end
+  
+  attr_accessor :isbn
+  attr_accessor :price
+
+  
+  def isbn
+    @isbn
+  end
+  
+  def price
+    @price
+  end
+  
+  def price_as_string
+    "$%0.2f" % @price
+  end
 end
